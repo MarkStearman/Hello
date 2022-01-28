d063c94bc9a8a45f6b2e5740120af435a6cbb42c
Mark Stearman <mstearman2@husters.unl.edu>
2022-01-28 09:39:26 -0600

    adding hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * @file    hello.c
+ * @author  Mark Stearman
+ * @brief 
+ * @version 0.1
+ * @date    2022-01-28
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
